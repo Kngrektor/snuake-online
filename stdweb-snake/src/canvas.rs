9ce6b42d474eedbd799e33f970be2b111b95abbe
johesoman <johesoman@gmail.com>
2019-03-16 13:02:06 +0100

    code: added canvas, .toml: added deps and changed username

---
+++ stdweb-snake/src/canvas.rs
@@ -0,0 +1,97 @@
+use stdweb::traits::*;
+use stdweb::unstable::TryInto;
+use stdweb::web::html_element::CanvasElement;
+use stdweb::web::{document, CanvasRenderingContext2d};
+
+pub struct Canvas {
+    pub canvas: CanvasElement,
+    pub ctx: CanvasRenderingContext2d,
+}
+
+impl Canvas {
+    pub fn new(attr_id: &str) -> Self {
+        let canvas: CanvasElement = document()
+            .query_selector(attr_id)
+            .unwrap()
+            .unwrap()
+            .try_into()
+            .unwrap();
+
+        let ctx: CanvasRenderingContext2d = canvas.get_context().unwrap();
+
+        Canvas {
+            canvas,
+            ctx,
+        }
+    }
+
+    pub fn width(&self) -> u32 { self.canvas.width() }
+
+    pub fn height(&self) -> u32 { self.canvas.height() }
+
+    pub fn draw_rect(&self, x: u32, y: u32, w: u32, h: u32, color: &str) {
+        self.ctx.set_fill_style_color(color);
+
+        self.ctx.fill_rect(x.into(), y.into(), w.into(), h.into());
+    }
+
+    pub fn clear(&self, color: &str) {
+        self.ctx.set_fill_style_color(color);
+        self.ctx.fill_rect(
+            0.0,
+            0.0,
+            self.canvas.width().into(),
+            self.canvas.height().into(),
+        );
+    }
+}
+
+pub struct GridCanvas<'a> {
+    canvas: &'a Canvas,
+    scaled_width: u32,
+    scaled_height: u32,
+    cols: u32,
+    rows: u32,
+}
+
+impl<'a> GridCanvas<'a> {
+    pub fn new(canvas: &'a Canvas, rows: u32, cols: u32) -> Self {
+        let scaled_width = canvas.width() / cols;
+        let scaled_height = canvas.height() / rows;
+
+        Self {
+            canvas,
+            scaled_width,
+            scaled_height,
+            cols,
+            rows,
+        }
+    }
+
+    pub fn draw_at(&self, x: u32, y: u32, color: &str) {
+        assert!(x < self.cols);
+        assert!(y < self.rows);
+
+        let x = x * self.scaled_width;
+        let y = y * self.scaled_height;
+
+        self.canvas.draw_rect(
+            x,
+            y,
+            self.scaled_width,
+            self.scaled_height,
+            color,
+        )
+    }
+
+    pub fn clear(&self, color: &str) {
+        self.canvas.clear(color);
+    }
+}
+
+impl Canvas {
+    pub fn grid_canvas<'a>(&'a self, rows: u32, cols: u32) -> GridCanvas<'a> {
+        GridCanvas::new(self, rows, cols)
+    }
+}
+
--- stdweb-snake/src/main.rs
+++ stdweb-snake/src/main.rs
@@ -1,3 +1,19 @@
+#[macro_use]
+extern crate stdweb;
+
+mod canvas;
+use crate::canvas::Canvas;
+
 fn main() {
-    println!("Hello, world!");
+    stdweb::initialize();
+
+    let canvas = Canvas::new("#canvas");
+    let gc = canvas.grid_canvas(20, 20);
+
+    gc.clear("black");
+    gc.draw_at(0, 0, "red");
+    gc.draw_at(1, 1, "orange");
+    gc.draw_at(19, 19, "blue");
+
+    stdweb::event_loop();
 }
